5fa3fd04d822883bf922d72de461632a99d50e84
Sreggie <darren.rogers06@gmail.com>
2022-08-18 18:02:22 -0400

    password generator working

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # JavaScript-Password-Generator
+
+## Description
+
+## Visuals
--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,48 @@
 // Assignment code here
 
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+function generatePassword(){
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz"
+  var upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  var symbols ='#$%&()*+,-./:;<=>?@[\]^_`{|}~'
+  var numbers = '1234567890'
+  var hasLower = confirm("Will your password have lowercase?");
+  var hasUpper = confirm("Will your password have uppercase?");
+  var hasSymbol = confirm("Will your password have symbols?");
+  var hasNumber = confirm("Will your password have numbers?");
+  var characters = prompt('How many character will your password include')
+  if(characters < 8 || characters >128 ){
+    alert('Characters must be more than 8 and less than 128');
+  }
+  possiblepass=''
+  currentpassword=''
+  if(hasLower){
+    possiblepass +=lowerCase
+  }
+  if(hasUpper){
+    possiblepass +=upperCase
+  }
+  if(hasSymbol){
+    possiblepass +=symbols
+  }
+  if(hasNumber){
+    possiblepass +=numbers
+  }
+  for(var i=0; i < characters; i++){
+    var random = Math.floor(Math.random()* possiblepass.length)
+    currentpassword += possiblepass[random]
+  }
+  console.log(currentpassword);
+  return(currentpassword);
+}
+
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
